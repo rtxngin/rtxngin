857c776ee86ba02d451db4774eb5d7b97fa70864
Dashbloxx <dashbloxx@gmail.com>
2023-12-31 20:49:33 -0500

    Implement vector3!

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 
+#include "color.hpp"
+#include "vector3.hpp"
+
 int main()
 {
 	int image_width = 256;
@@ -12,17 +15,13 @@ int main()
 
 	for(int j = 0; j < image_height; ++j)
 	{
+		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
 		for(int i = 0; i < image_width; ++i)
 		{
-			auto r = double(i) / (image_width-1);
-			auto g = double(j) / (image_height-1);
-			auto b = 0;
-
-			int ir = static_cast<int>(255.999 * r);
-			int ig = static_cast<int>(255.999 * g);
-			int ib = static_cast<int>(255.999 * b);
-
-			image << ir << ' ' << ig << ' ' << ib << '\n';
+			auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+			write_color(std::cout, pixel_color);
 		}
 	}
+
+	std::clog << "\rDone.                 \n";
 }
