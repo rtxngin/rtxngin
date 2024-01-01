82f9973ab52d1f4cabbdc8819f0fa42d28656ae0
Dashbloxx <dashbloxx@gmail.com>
2023-12-31 22:36:56 -0500

    Implement anti-aliasing!

--- source/rtweekend.hpp
+++ source/rtweekend.hpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
 
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
@@ -10,4 +11,16 @@ const double pi = 3.1415926535897932385;
 inline double degrees_to_radians(double degrees)
 {
 	return degrees * pi / 180.0;
+}
+
+inline double random_double()
+{
+	static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+	static std::mt19937 generator;
+	return distribution(generator);
+}
+
+inline double random_double(double min, double max)
+{
+	return min + (max-min)*random_double();
 }
