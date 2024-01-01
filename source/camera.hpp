9b72b27dc22d826edbcf3a90a91eb84487540654
Dashbloxx <dashbloxx@gmail.com>
2024-01-01 09:48:45 -0500

    Finish raytracer!

--- source/camera.hpp
+++ source/camera.hpp
@@ -13,6 +13,15 @@ public:
 	int samples_per_pixel = 10;
 	int max_depth = 10;
 
+	/* Field of view. */
+	double vfov = 90;
+	point3 lookfrom = point3(0, 0, -1);
+	point3 lookat = point3(0, 0, 0);
+	vec3 vup = vec3(0, 1, 0);
+
+	double defocus_angle = 0;
+	double focus_dist = 10;
+
 	void render(const hittable& world)
 	{
 		initialize();
@@ -24,7 +33,7 @@ public:
 			std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
 			for(int i = 0; i < image_width; ++i)
 			{
-				color pixel_color(0,0,0);
+				color pixel_color(0, 0, 0);
 				for (int sample = 0; sample < samples_per_pixel; ++sample) {
 					ray r = get_ray(i, j);
 					pixel_color += ray_color(r, max_depth, world);
@@ -42,33 +51,45 @@ private:
 	point3 pixel00_loc;
 	vec3 pixel_delta_u;
 	vec3 pixel_delta_v;
+	vec3 u, v, w;
+	vec3 defocus_disk_u;
+	vec3 defocus_disk_v;
 
 	void initialize()
 	{
 		image_height = static_cast<int>(image_width / aspect_ratio);
 		image_height = (image_height < 1) ? 1 : image_height;
 
-		center = point3(0, 0, 0);
+		center = lookfrom;
 
-		auto focal_length = 1.0;
-		auto viewport_height = 2.0;
-		auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
+		auto theta = degrees_to_radians(vfov);
+		auto h = tan(theta / 2);
+		auto viewport_height = 2 * h * focus_dist;
+		auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
 
-		auto viewport_u = vec3(viewport_width, 0, 0);
-		auto viewport_v = vec3(0, -viewport_height, 0);
+		w = unit_vector(lookfrom - lookat);
+		u = unit_vector(cross(vup, w));
+		v = cross(w, u);
+
+		vec3 viewport_u = viewport_width * u;
+		vec3 viewport_v = viewport_height * -v;
 
 		pixel_delta_u = viewport_u / image_width;
 		pixel_delta_v = viewport_v / image_height;
 
-		auto viewport_upper_left = center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+		auto viewport_upper_left = center - (focus_dist * w) - viewport_u / 2 - viewport_v / 2;
 		pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+		auto defocus_radius = focus_dist * tan(degrees_to_radians(defocus_angle / 2));
+		defocus_disk_u = u * defocus_radius;
+		defocus_disk_v = v * defocus_radius;
 	}
 
 	ray get_ray(int i, int j) const {
 		auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
 		auto pixel_sample = pixel_center + pixel_sample_square();
 
-		auto ray_origin = center;
+		auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
 		auto ray_direction = pixel_sample - ray_origin;
 
 		return ray(ray_origin, ray_direction);
@@ -80,23 +101,28 @@ private:
 		return (px * pixel_delta_u) + (py * pixel_delta_v);
 	}
 
+	point3 defocus_disk_sample() const {
+		auto p = random_in_unit_disk();
+		return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
+	}
+
 	color ray_color(const ray& r, int depth, const hittable& world) const {
 		hit_record rec;
 
 		if (depth <= 0)
-			return color(0,0,0);
+			return color(0, 0, 0);
 
 		if (world.hit(r, interval(0.001, infinity), rec))
 		{
 			ray scattered;
 			color attenuation;
 			if (rec.mat->scatter(r, rec, attenuation, scattered))
-				return attenuation * ray_color(scattered, depth-1, world);
+				return attenuation * ray_color(scattered, depth - 1, world);
 			return color(0,0,0);
 		}
 
 		vec3 unit_direction = unit_vector(r.direction());
-		auto a = 0.5*(unit_direction.y() + 1.0);
-		return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+		auto a = 0.5 * (unit_direction.y() + 1.0);
+		return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 	}
 };
--- source/main.cpp
+++ source/main.cpp
@@ -14,14 +14,16 @@ int main()
 {
 	hittable_list world;
 
-	auto material_ground = std::make_shared<lambertian>(color(0.0, 0.0, 0.0));
-	auto material_center = std::make_shared<lambertian>(color(0.7, 0.3, 0.3));
-	auto material_left   = std::make_shared<metal>(color(0.8, 0.8, 0.8));
-	auto material_right  = std::make_shared<metal>(color(0.8, 0.6, 0.2));
+	auto material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
+	auto material_center = std::make_shared<lambertian>(color(0.1, 0.2, 0.5));
+	auto material_left = std::make_shared<dielectric>(1.5);
+	auto material_right = std::make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
-	world.add(std::make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, std::make_shared<lambertian>(color(0.2, 0.2, 0.2))));
-	world.add(std::make_shared<sphere>(point3( 0.0, 0.0, -1.0), 0.5, std::make_shared<lambertian>(color(0.7, 0.3, 0.3))));
-	world.add(std::make_shared<sphere>(point3( 1.0, 1.0, -1.0), 0.5, std::make_shared<metal>(color(0.1, 0.1, 0.1))));
+	world.add(std::make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+	world.add(std::make_shared<sphere>(point3( 0.0, 0.0, -1.0), 0.5, material_center));
+	world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
+	world.add(std::make_shared<sphere>(point3( 1.0, 0.0, -1.0), 0.5, material_right));
 
 	camera cam;
 
@@ -29,6 +31,14 @@ int main()
 	cam.image_width = 400;
 	cam.samples_per_pixel = 100;
 	cam.max_depth = 50;
+
+	cam.vfov = 90;
+	cam.lookfrom = point3(-2, 2, 1);
+	cam.lookat = point3(0, 0, -1);
+	cam.vup = vec3(0, 1, 0);
+
+	cam.defocus_angle = 10.0;
+	cam.focus_dist = 3.4;
 
 	cam.render(world);
 }
