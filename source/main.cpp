e9ba35d845d964832aefb038d8dbfac5c6d592e0
Dashbloxx <dashbloxx@gmail.com>
2023-12-31 22:02:40 -0500

    Get red sphere drawing!

--- source/main.cpp
+++ source/main.cpp
@@ -3,11 +3,49 @@
 
 #include "color.hpp"
 #include "vector3.hpp"
+#include "ray.hpp"
+
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+	vec3 oc = r.origin() - center;
+	auto a = dot(r.direction(), r.direction());
+	auto b = 2.0 * dot(oc, r.direction());
+	auto c = dot(oc, oc) - radius * radius;
+	auto discriminant = b * b - 4 * a * c;
+	return (discriminant >= 0);
+}
+
+color ray_color(const ray& r)
+{
+	if(hit_sphere(point3(0, 0, -1), 0.5, r))
+		return color(1, 0, 0);
+
+	vec3 unit_direction = unit_vector(r.direction());
+	auto a = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
 
 int main()
 {
-	int image_width = 256;
-	int image_height = 256;
+	auto aspect_ratio = 16.0 / 9.0;
+	int image_width = 400;
+
+	int image_height = static_cast<int>(image_width / aspect_ratio);
+	image_height = (image_height < 1) ? 1 : image_height;
+
+	auto focal_length = 1.0;
+	auto viewport_height = 2.0;
+	auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
+	auto camera_center = point3(0, 0, 0);
+
+	auto viewport_u = vec3(viewport_width, 0, 0);
+	auto viewport_v = vec3(0, -viewport_height, 0);
+
+	auto pixel_delta_u = viewport_u / image_width;
+	auto pixel_delta_v = viewport_v / image_height;
+
+	auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+	auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
 	std::ofstream image("image.ppm");
 
@@ -18,8 +56,12 @@ int main()
 		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
 		for(int i = 0; i < image_width; ++i)
 		{
-			auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
-			write_color(std::cout, pixel_color);
+			auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+			auto ray_direction = pixel_center - camera_center;
+			ray r(camera_center, ray_direction);
+
+			color pixel_color = ray_color(r);
+			write_color(image, pixel_color);
 		}
 	}
 
